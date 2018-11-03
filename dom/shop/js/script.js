e0dc0374257e171990e2c2ad8a732c5c8520886d
Elena Anischenkova <eanischenkova@gmail.com>
2018-11-03 16:14:06 +0300

    hw4: shop

--- dom/shop/js/script.js
+++ dom/shop/js/script.js
@@ -5,6 +5,31 @@ class Basket {
     if (!(container instanceof Element) || typeof helper !== 'function') {
       return;
     }
+
+    this.container = container;
+    this.helper = helper;
+
+    this.btnsAdd = this.container.querySelectorAll('.add');
+    this.cartCount = this.container.querySelector('#cart-count');
+    this.totalPrice = this.container.querySelector('#cart-total-price');
+
+    this.intermediatePrice = 0;
+    this.btnsAdd.forEach(btn => btn.addEventListener('click', this.addProduct.bind(this)))
+  }
+
+  addProduct(event) {
+    const price = event.target.getAttribute('data-price');
+    this.setCount();
+    this.setPrice(price);
+  }
+
+  setCount() {
+    this.cartCount.innerHTML++;
+  }
+
+  setPrice(price) {
+    this.intermediatePrice += +price;
+    this.totalPrice.innerHTML = this.helper(this.intermediatePrice);
   }
 }
 

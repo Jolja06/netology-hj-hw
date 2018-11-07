ac88fa72cba36983e29859f97248b9b6eaf97d5f
Елена Анищенкова <anischenkovaev@altarix.ru>
2018-11-07 16:51:48 +0300

    hw6: currency converter

---
+++ html-forms/currency-converter/js/main.js
@@ -0,0 +1,73 @@
+'use strict';
+const isURL = /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/;
+class Converter {
+  constructor(container, url) {
+    if (!(container instanceof Element) && !(isURL.test(url))) {
+      return;
+    }
+
+    this.amount = this.input.value;
+    this.container = container;
+    this.currency = [];
+    this.from = this.container.querySelector('#from');
+    this.input = this.container.querySelector('#source');
+    this.loader = document.querySelector('#loader');
+    this.output = this.container.querySelector('#result');
+    this.selects = this.container.querySelectorAll('select');
+    this.to = this.container.querySelector('#to');
+    this.url = url;
+    this.xhr = new XMLHttpRequest();
+
+    this.xhr.addEventListener('load', this.handleLoad.bind(this));
+    this.input.addEventListener('input', this.handleConversion.bind(this));
+    this.from.addEventListener('change', this.handleConversion.bind(this));
+    this.to.addEventListener('change', this.handleConversion.bind(this));
+
+    this.fetchContent();
+  }
+
+  fetchContent() {
+    this.xhr.open('GET', this.url, true);
+    this.xhr.send();
+    this.toggleLoader(true);
+  }
+
+  handleConversion() {
+    this.amount = this.input.value;
+    this.output.value = (this.from.value * this.amount / this.to.value).toFixed(2);
+  }
+
+  handleLoad() {
+    this.currency = JSON.parse(this.xhr.responseText);
+    this.toggleLoader(false);
+    this.render();
+  }
+
+  renderCurrency(currency) {
+    this.selects.forEach(select => {
+      const option = document.createElement('option');
+      option.value = currency.value;
+      option.label = currency.code;
+      option.title = currency.title;
+      select.appendChild(option);
+    });
+    this.handleConversion();
+  }
+
+  render() {
+    this.currency.forEach(this.renderCurrency.bind(this));
+  }
+
+  toggleLoader(isForce) {
+    this.container.classList.toggle('hidden', isForce);
+    this.loader.classList.toggle('hidden', !isForce);
+  }
+
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  new Converter(
+    document.querySelector('#content'),
+    'https://neto-api.herokuapp.com/currency'
+  );
+});

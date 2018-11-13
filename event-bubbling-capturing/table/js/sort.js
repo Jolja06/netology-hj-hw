c204037e85a354e5b19f6481f73b68e420807428
Елена Анищенкова <anischenkovaev@altarix.ru>
2018-11-13 13:29:13 +0300

    hw8: table

--- event-bubbling-capturing/table/js/sort.js
+++ event-bubbling-capturing/table/js/sort.js
@@ -1,5 +1,12 @@
 'use strict';
-
+let isSorted = false;
 function handleTableClick(event) {
-  
+  const target = event.target;
+  if (target.dataset.propName) {
+    target.dataset.dir = !(isSorted) ? -1 : 1;
+    isSorted = !isSorted;
+  }
+  target.closest('table').dataset.sortBy = target.dataset.propName;
+
+  sortTable(target.dataset.propName, target.dataset.dir);
 }

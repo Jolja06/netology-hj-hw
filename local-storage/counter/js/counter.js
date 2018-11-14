299c71733427945d8322de8ac1257d6a58013945
Елена Анищенкова <anischenkovaev@altarix.ru>
2018-11-14 13:05:02 +0300

    hw9: counter

--- local-storage/counter/js/counter.js
+++ local-storage/counter/js/counter.js
@@ -1,1 +1,47 @@
 'use strict';
+
+class Counter {
+  constructor(container) {
+    if (!(container instanceof Element)) {
+      return;
+    }
+    this.count = localStorage.count;
+    this.container = container;
+    this.view = this.container.querySelector('#counter');
+    this.buttons = this.container.querySelector('.wrap-btns');
+
+    this.init();
+  }
+
+  init() {
+    this.buttons.addEventListener('click', this.handleChange.bind(this));
+    this.render();
+  }
+
+  handleChange(event) {
+    const target = event.target;
+    switch (target.id) {
+      case 'increment' :
+        this.count = localStorage.count = +this.count + 1;
+        break;
+      case 'decrement' :
+        this.count = localStorage.count = +this.count - 1;
+        break;
+      case 'reset' :
+        this.count = localStorage.count = '0';
+        break;
+
+      default:
+        break;
+    }
+    this.render();
+  }
+
+  render() {
+    this.view.innerText = this.count;
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  new Counter(document.querySelector('.wrap'));
+});

f223617e5c3f498030921f871c885dc4337dabf6
Елена Анищенкова <anischenkovaev@altarix.ru>
2018-11-19 13:10:45 +0300

    hw10: bubbles

--- websocket/bubbles/js/click.js
+++ websocket/bubbles/js/click.js
@@ -1,1 +1,30 @@
 'use strict';
+const isServer = /^(wss?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/;
+class Bubbles {
+  constructor(server) {
+    if (!(isServer.test(server))) {
+      return;
+    }
+    this.server = server;
+    this.connection = new WebSocket(this.server);
+
+    this.click = document.addEventListener('click', this.handleClick.bind(this));
+
+    this.init();
+  }
+
+  init() {
+    this.connection.addEventListener('open', () => showBubbles(this.connection));
+  }
+
+  handleClick(event) {
+    this.connection.send(JSON.stringify({
+      x: event.x,
+      y: event.y,
+    }))
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  new Bubbles('wss://neto-api.herokuapp.com/mouse');
+});

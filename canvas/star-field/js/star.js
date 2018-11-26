59cb5d9e3b9c92045df0c3ec921eeba1da4d940c
Елена Анищенкова <anischenkovaev@altarix.ru>
2018-11-26 10:51:44 +0300

    hw12: star-field

---
+++ canvas/star-field/js/star.js
@@ -0,0 +1,50 @@
+'use strict';
+
+class Stars {
+  constructor(canvas) {
+    if (canvas.tagName !== 'CANVAS') {
+      return;
+    }
+    this.canvas = canvas;
+    this.ctx = canvas.getContext('2d');
+    this.number = 200;
+    this.colors = [ '#ffffff', '#ffe9c4', '#d4fbff' ];
+
+    this.canvas.addEventListener('click', this.render.bind(this))
+    this.init();
+  }
+
+  init() {
+    this.canvas.style.backgroundColor = '#000000';
+    this.render();
+  }
+
+  randomNumber() {
+    this.number = Math.round( 199.5 + Math.random() * 201);
+  }
+
+  renderStar() {
+    const radius = (Math.random() * 1.1) / 2;
+    const x = Math.random() * 800;
+    const y = Math.random() * 400;
+    const randomColor = Math.round(-0.5 + Math.random() * 3);
+
+    this.ctx.beginPath();
+    this.ctx.globalAlpha = 0.8 + Math.random() * (1 - 0.8);
+    this.ctx.fillStyle = this.colors[randomColor];
+    this.ctx.arc(x, y, radius, 0, 2 * Math.PI);
+    this.ctx.fill();
+  }
+
+  render() {
+    this.randomNumber();
+    this.ctx.clearRect(0, 0, 800, 400);
+    for (let i = 0; i <= this.number; i++) {
+      this.renderStar();
+    }
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  new Stars(document.querySelector('canvas'))
+});

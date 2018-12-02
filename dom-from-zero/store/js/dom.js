2e9b42bfa4ad7252a8d0006f02bb4cfda99fc1f8
Elena Anischenkova <eanischenkova@gmail.com>
2018-12-02 21:26:40 +0300

    hw13 store

--- dom-from-zero/store/js/dom.js
+++ dom-from-zero/store/js/dom.js
@@ -1,1 +1,24 @@
 'use strict';
+
+function createElement(node) {
+	const { name, props, childs } =  node;
+
+	if (typeof node === 'string') {
+		return document.createTextNode(node);
+	}
+
+	const elem = document.createElement(name);
+	
+	if (typeof props === 'object' && props !== null) {
+		Object.keys(props).forEach(attr => elem.setAttribute(attr, props[attr]));
+	}
+
+	if (childs instanceof Array) {
+		childs.forEach(child => {
+			const childElem = createElement(child);
+			elem.appendChild(childElem);
+		});
+	}
+
+	return elem;
+}

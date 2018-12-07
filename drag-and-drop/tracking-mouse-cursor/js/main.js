3129b6efa29ce15a341e9b3e988bbc9d25df30b3
Elena Anischenkova <eanischenkova@gmail.com>
2018-12-07 18:31:48 +0300

    some feractoring

--- drag-and-drop/tracking-mouse-cursor/js/main.js
+++ drag-and-drop/tracking-mouse-cursor/js/main.js
@@ -1,39 +1,44 @@
 'use strict';
 
 class Eye {
-	constructor(eye) {
-		if (!(eye instanceof Element)) return;
+  constructor(eye) {
+    if (!(eye instanceof Element)) return;
 
-		document.addEventListener('mousemove', (event) => this.throttle(this.movement(event)));
-		this.eye = eye;
-	}
+    this.eye = eye;
 
-  movement(event) {
-			const {left, top, width, height} = this.eye.getBoundingClientRect();
-			const x = left + width / 2;
-			const y = top + height / 2;
-			const rad = Math.atan2(event.pageX - x, event.pageY - y);
-			const rotation = (rad * (180 / Math.PI) * -1) + 180;
+    this.move = this.throttle(this.move.bind(this));
 
-			this.eye.style.transform = `rotate(${rotation}deg)`;
-	}
+    document.addEventListener('mousemove', this.move);
 
-	throttle(callback) {
-		let isWaiting = false;
-		return function() {
-			if (!isWaiting) {
-				callback.apply(this, arguments);
-				isWaiting = true;
-				requestAnimationFrame(() => {
-					isWaiting = false;
-				});
-			}
-		}
-	}
+  }
+
+  move(event) {
+    const {left, top, width, height} = this.eye.getBoundingClientRect();
+
+    const x = left + (width / 2);
+    const y = top + (height / 2);
+    const rad = Math.atan2(event.pageY - y, event.pageX - x);
+    const rotation = rad * (180 / Math.PI) - 45;
+
+    this.eye.style.transform = `rotate(${rotation}deg)`;
+  }
+
+  throttle(callback) {
+    let isWaiting = false;
+    return function() {
+      if (!isWaiting) {
+        callback.apply(this, arguments);
+        isWaiting = true;
+        requestAnimationFrame(() => {
+          isWaiting = false;
+        });
+      }
+    }
+  }
 
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-	new Eye(document.querySelector('.cat_position_for_right_eye'));
-	new Eye(document.querySelector('.cat_position_for_left_eye'));
+  new Eye(document.querySelector('.cat_position_for_right_eye'));
+  new Eye(document.querySelector('.cat_position_for_left_eye'));
 });
